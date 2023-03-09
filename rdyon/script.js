3b103ad3076e52b30fa72d2439dee98f687aa46d
scrxvv <dimag0767@gmail.com>
2023-03-09 22:13:37 +0400

    end

--- rdyon/script.js
+++
@@ -1,8 +0,0 @@
-const iconMenu = document.querySelector('.adaptive-icon');
-if (iconMenu) {
-    const menuBody = document.querySelector('.menu');
-    iconMenu.addEventListener("click", function (e) {
-        iconMenu.classList.toggle('_active')
-        menuBody.classList.toggle('_active')
-    });
-}
